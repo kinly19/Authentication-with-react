b3695735b9cddcd75bbdbd4d2835bece359cdfc5
unknown <kinchoily@hotmail.com>
2021-11-17 18:55:03 +0000

    AuthForm.js - Create submitHandler for form onSubmit

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -15,6 +15,8 @@ const AuthForm = () => {
   //storing inputs with useRef
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
+  const fireBaseKey = 'AIzaSyDXQZiKmN9NITd8O6cnXccudiKDrftom4k';
+  
   const [isLogin, setIsLogin] = useState(true);
 
   //switch state 
@@ -22,10 +24,64 @@ const AuthForm = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  //submit form
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // console.log( 'Email:' + emailInputRef.current.value + ' Password:' + passwordInputRef.current.value);
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    //optional validate input here
+    setIsloading(true);
+    let url;
+    //change url endpoint for either login or signup
+    if (isLogin) {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${fireBaseKey}`
+    } else {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${fireBaseKey}` 
+    }
+
+    fetch(url, {
+      method: "POST",
+      //data to send, JSON.stringify()
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      //ensures REST api knows we have JSON data coming in
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((res) => {
+        setIsloading(false);
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            // generic/default message if below check fails
+            // or just show a custom error modal etc
+            let errorMessage = "Authentication failed";
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1> {/*ternary operator*/}
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
           <input type='email' id='email' required ref={emailInputRef}/>
