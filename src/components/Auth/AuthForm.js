73d6d943583028c5dc3a2b69ec10c2e80162de19
unknown <kinchoily@hotmail.com>
2021-11-17 13:38:24 +0000

    AuthForm.js - Storing user input values with useRef

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
@@ -11,6 +11,10 @@ import classes from './AuthForm.module.css';
 // ===================================================================
 
 const AuthForm = () => {
+
+  //storing inputs with useRef
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
 
   //switch state 
@@ -24,11 +28,11 @@ const AuthForm = () => {
       <form>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailInputRef}/>
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input type='password' id='password' required ref={passwordInputRef}/>
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
