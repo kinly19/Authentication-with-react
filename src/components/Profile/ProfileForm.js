45f8904d9b319212fc1557963ef14530b4d7c890
unknown <kinchoily@hotmail.com>
2021-11-18 14:54:55 +0000

    ProfileForm.js - Add handler to allow user to change their password

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,56 @@
+import { useRef, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import classes from './ProfileForm.module.css';
+import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
+
+  const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext); 
+  const fireBaseKey = 'AIzaSyDXQZiKmN9NITd8O6cnXccudiKDrftom4k';
+  const history = useHistory();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const enteredNewPassword = newPasswordInputRef.current.value;
+    console.log(enteredNewPassword);
+    console.log(authCtx.token);
+
+    fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${fireBaseKey}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredNewPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+          history.replace("/");
+          alert("Password Changed");
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = data.error.message;
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .catch((err) => {
+        alert(err.message);
+      });  
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' ref={newPasswordInputRef}/>
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
